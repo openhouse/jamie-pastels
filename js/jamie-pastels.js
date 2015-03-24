3e2ca86f4ad96719c32e213c35f452e2577bbdce
Jamie Burkart <jamie@wowlist.org>
2015-03-24 14:44:58 -0400

    add jamie-pastels.js to look up hex color from color name

---
+++ js/jamie-pastels.js
@@ -0,0 +1,56 @@
+/*------------------------------------*\
+    Jamie Pastels
+    49 colors of fun
+\*------------------------------------*/
+
+var jamiePastels = [];
+
+jamiePastels['jp-white'] =              '#eeefec';
+jamiePastels['jp-pale-orange'] =        '#fce1d1';
+jamiePastels['jp-lemon-yellow'] =       '#f7ec86';
+jamiePastels['jp-naples-yellow'] =      '#fae367';
+jamiePastels['jp-pale-green'] =         '#beebc7';
+jamiePastels['jp-yellow'] =             '#ffdb06';
+jamiePastels['jp-chrome-yellow'] =      '#ffd323';
+jamiePastels['jp-pale-orange-2'] =      '#fec9b7';
+jamiePastels['jp-pale-brown'] =         '#efb884';
+jamiePastels['jp-yellow-orange'] =      '#ffb101';
+jamiePastels['jp-pink'] =               '#f4a9c5';
+jamiePastels['jp-yellow-ochre'] =       '#e9b64e';
+jamiePastels['jp-salmon-pink'] =        '#ffa77f';
+jamiePastels['jp-classic-rose'] =       '#e2aeb4';
+jamiePastels['jp-pale-blue'] =          '#74c2e5';
+jamiePastels['jp-gray-2'] =             '#b4b3b1';
+jamiePastels['jp-pale-vermillion'] =    '#fe998f';
+jamiePastels['jp-yellow-gray'] =        '#bbab8c';
+jamiePastels['jp-yellow-green'] =       '#79c039';
+jamiePastels['jp-gray'] =               '#a2a5a6';
+jamiePastels['jp-okre'] =               '#da8f1f';
+jamiePastels['jp-orange'] =             '#f77744';
+jamiePastels['jp-green'] =              '#34a63c';
+jamiePastels['jp-blue'] =               '#1098d9';
+jamiePastels['jp-olive-green'] =        '#949136';
+jamiePastels['jp-hyacinth-violet'] =    '#977db5';
+jamiePastels['jp-green-gray'] =         '#568e62';
+jamiePastels['jp-crimson-lake'] =       '#d45462';
+jamiePastels['jp-vermillion'] =         '#e04d38';
+jamiePastels['jp-blue-green'] =         '#018c85';
+jamiePastels['jp-deep-gray'] =          '#6f7d7e';
+jamiePastels['jp-rose-madder'] =        '#d04667';
+jamiePastels['jp-ultramarine-light'] =  '#4e75b7';
+jamiePastels['jp-scarlet'] =            '#d73d36';
+jamiePastels['jp-red'] =                '#cd3031';
+jamiePastels['jp-brown'] =              '#a55237';
+jamiePastels['jp-cobalt-blue'] =        '#0167b2';
+jamiePastels['jp-purple'] =             '#944a83';
+jamiePastels['jp-dark-gray'] =          '#5e5f61';
+jamiePastels['jp-vandyke-brown'] =      '#76533a';
+jamiePastels['jp-indian-red'] =         '#764c42';
+jamiePastels['jp-violet'] =             '#624c7f';
+jamiePastels['jp-deep-green'] =         '#1f5c3e';
+jamiePastels['jp-ultramarine'] =        '#30499a';
+jamiePastels['jp-prussian-blue-light'] = '#015482';
+jamiePastels['jp-prussian-blue'] =      '#014f94';
+jamiePastels['jp-dark-carmine'] =       '#772d24';
+jamiePastels['jp-dark-green'] =         '#2b463e';
+jamiePastels['jp-black'] =              '#343435';
